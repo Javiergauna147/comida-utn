75ae5337bd3f6c8620f183c22f41bc999a1a126a
Javier <javier.gauna147@gmail.com>
2022-11-22 22:28:53 -0300

    premitiendo seleccionar domicilio en carrito

--- src/app/modules/products/carrito-compras-page/carrito-compras-page.component.ts
+++ src/app/modules/products/carrito-compras-page/carrito-compras-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Articulo } from 'src/app/services/articulos/articulo.interface';
+import { Domicilio } from 'src/app/services/usuarios/usuario.interface';
+import { UsuarioService } from '../../../services/usuarios/usuario.service';
 
 @Component({
   selector: 'app-carrito-compras-page',
@@ -10,10 +12,21 @@ export class CarritoComprasPageComponent implements OnInit {
 
   carritoCompra: {cantidad: number, articulo: Articulo}[];
 
-  constructor() {
+  domicilioSelected: Domicilio;
+
+  domicilios: Domicilio[] = this.usuarioService.obtenerUsuario().domicilios;
+
+  constructor(private usuarioService: UsuarioService) {
     this.carritoCompra = JSON.parse(localStorage.getItem('carrito') || '[]')
+    console.log("domicilios disponibles");
+    console.log(this.domicilios);
   }
+
   ngOnInit(): void {
+  }
+  
+  crearPedido() {
+    console.log(this.domicilioSelected);
   }
 
 }
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -4,6 +4,7 @@ import { CatalogoPageComponent } from './catalogo-page/catalogo-page.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { CarritoComprasPageComponent } from './carrito-compras-page/carrito-compras-page.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +16,8 @@ import { CarritoComprasPageComponent } from './carrito-compras-page/carrito-comp
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    FormsModule
   ]
 })
 export class ProductsModule { }
